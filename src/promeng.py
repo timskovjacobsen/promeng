19b5c8e093ed4670a37acd1cd93ab283c997c5db
Tim Skov Jacobsen <timskovjacobsen@gmail.com>
2021-06-15 22:57:47 +0200

    Add Cart and CartItem classes

--- src/promeng.py
+++ src/promeng.py
@@ -19,3 +19,28 @@ class PromotionByVariety:
     skus: List[StockKeepingUnit]
     price: float
 
+
+
+@dataclass
+class CartItem:
+    sku: StockKeepingUnit
+    quantity: int
+
+    @property
+    def id_(self):
+        return self.sku.id_
+
+
+class Cart:
+    def __init__(self, items: List[CartItem] = None):
+        if not items:
+            self.items = []
+        else:
+            self.items = items
+
+        # Create a dictionary for easy accessing via SKU ID
+        self._items_dict = {cartitem.id_: cartitem for cartitem in self.items}
+
+    def __getitem__(self, id_: str) -> CartItem:
+        return self._items_dict[id_]
+
