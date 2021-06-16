14ec2a5d468b01ec017e6596d2192dd611873ed3
Tim Skov Jacobsen <timskovjacobsen@gmail.com>
2021-06-17 00:24:45 +0200

    Correct test for PromotionByQuantity.apply

--- tests/test_promeng.py
+++ tests/test_promeng.py
@@ -36,17 +36,19 @@ def test_scanrioA():
     assert actual == expected
 
 
-def test_PromotionsByQuantity_apply_method():
+def test_PromotionByQuantity_apply_method():
     # ---- Setup ----
     A = StockKeepingUnit(id_="A", price=50)
-    B = StockKeepingUnit(id_="B", price=50)
+    B = StockKeepingUnit(id_="B", price=30)
 
     # ---- Execute -----
     promotion = PromotionByQuantity(sku=A, quantity=3, price=130)
 
+    promo_skus, _ = promotion.filter([A, A, B, B, A, A, A, B, B, A, A])
+
     # ---- Verify -----
-    actual = promotion.apply(skus=[A, A, B, B, A, A, A, B, B, A, A])
-    expected = 2 * 130 + 1 * 50
+    actual = promotion.apply(skus=promo_skus)
+    expected = 2 * 130
 
     assert actual == expected
 
