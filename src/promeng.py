ae8bd550b6ec6c2179c210e2724f4cd0b717b2fa
Tim Skov Jacobsen <timskovjacobsen@gmail.com>
2021-06-16 20:47:49 +0200

    Implement PromotionByQuantity.apply

--- src/promeng.py
+++ src/promeng.py
@@ -25,10 +25,18 @@ class PromotionByQuantity(PromotionInterface):
     price: float
 
     def apply(self, skus: List[StockKeepingUnit]) -> float:
-        pass
 
+        # Get the count of the SKUs mathching the ID of this promotion
+        ids = [sku.id_ for sku in skus]
+        id_count = ids.count(self.sku.id_)
 
-class PromotionByVariety:
+        # Compute price taking into account promotions
+        discount_groups, full_price_groups = divmod(id_count, self.quantity)
+        discount_items_price = discount_groups * self.price
+        full_items_price = full_price_groups * self.sku.price
+
+        return discount_items_price + full_items_price
+
 
 @dataclass
 class PromotionByVariety(PromotionInterface):
